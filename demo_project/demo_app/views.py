f4bbbb6f2822de37ae319fd2f89c7f915a54b1cb
govil <govgm30@gmail.com>
2023-10-07 18:21:13 +0530

    Added a new API in demo app

--- demo_project/demo_app/views.py
+++ demo_project/demo_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import response
 
 # Create your views here.
+
+class FirstAPI(APIView):
+    
+    def get(self,request)
+        return ResPonse(data={'detail':'This is get method of first API'})
