f8d8a70b3c06c4c7572625b1eb04d2751e1e1bd8
Govilm1234 <146510460+Govilm1234@users.noreply.github.com>
2023-10-07 18:30:54 +0530

    Modified the response of first API

--- demo_project/demo_app/views.py
+++ demo_project/demo_app/views.py
@@ -6,4 +6,4 @@ from rest_framework.response import response
 class FirstAPI(APIView):
     
     def get(self,request)
-        return ResPonse(data={'detail':'This is get method of first API'})
+        return ResPonse(data={'detail':'GET method of first API'})
